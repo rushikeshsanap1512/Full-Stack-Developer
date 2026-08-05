5651373716bba85dd4cdbb0d30c3ba78d8100e5e
Rushikesh Sanap <rushikeshsanap1512@gmail.com>
2026-08-05 10:36:13 +0530

    html, css and javascript basics

---
+++ harkirat-singh-cohort-3.0/week-01/week-01-basics-of-javascript/index.js
@@ -0,0 +1,52 @@
+// var a = 1;
+// a = "harkirat";
+// a = true;
+
+// console.log(a)
+
+// var a = 0;
+// while(1){
+//   a++;
+// }
+
+// let name = "John";
+// const age = 30;
+// var isStudent = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+
+// let user1 = "harkirat";
+// let user2 = "raman";
+// let user3 = "asd";
+
+// console.log(user1);
+
+// let users = ["harkirat", "raman", "asd"];
+// console.log(users[1]);
+
+// function sum(a, b){
+//   let totalSum = a + b;
+//   return totalSum;
+// }
+
+// let ans = sum(1, 2);
+// let ans2 = sum(3, 4);
+
+// console.log(ans);
+// console.log(ans2);
+
+// function canVote(age){
+//   if(age > 18){
+//     console.log("You are an adult.");
+//   }
+//   else{
+//     console.log("You are a minor.");
+//   }
+// }
+
+// canVote(19);
+
+let users = ["rushikesh", "rahul", "diljeet"];
