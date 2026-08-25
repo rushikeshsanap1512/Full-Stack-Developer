9876bc5af4175b2fad919eb6f88e1c3b667403c7
Rushikesh Sanap <rushikeshsanap1512@gmail.com>
2026-08-25 23:24:37 +0530

    Step 6 - Start server only when database is connected

--- harkirat-singh-cohort-3.0/week-08/week-8-course-selling-app/index.js
+++ harkirat-singh-cohort-3.0/week-08/week-8-course-selling-app/index.js
@@ -3,11 +3,17 @@ const app = express();
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const {adminRouter} = require("./routes/admin")
+const mongoose = require("mongoose");
+
 app.use("/api/v1/user", userRouter);
 app.use("api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
+async function main(){
+    await mongoose.connect("mongodb+srv://admin:Rushi%401512@cluster0.tnbw87l.mongodb.net/coursera-app");
+    
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000.");
+    });
+}
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
-});
