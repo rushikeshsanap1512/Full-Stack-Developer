65a93e37239c142e0c3ec9cd04c2d2e4d09c65ed
Rushikesh Sanap <rushikeshsanap1512@gmail.com>
2026-08-26 15:44:54 +0530

    Step 9 - Add admin auth endpoints

--- harkirat-singh-cohort-3.0/week-08/week-8-course-selling-app/routes/admin.js
+++ harkirat-singh-cohort-3.0/week-08/week-8-course-selling-app/routes/admin.js
@@ -1,21 +1,92 @@
 const { Router } = require("express");
 const { adminModel } = require("../db");
-
+const bcrypt = require("bcrypt");
+const z = require("zod");
 const adminRouter = Router();
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_SECRET = "rushikeshAdmin12345"
 
 adminRouter.post("/signup", async (req, res) => {
+    const requireBody = z.object({
+        email: z.string().min(3).max(100).refine((email) => email.includes("@")),
+        password: z.string().min(3).max(30),
+        firstName: z.string().min(2).max(30),
+        lastName: z.string().min(2).max(30)
+    });
+
+    const parseDataWithSuccess = requireBody.safeParse(req.body);
+
+    if (!parseDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect format",
+            error: parseDataWithSuccess.error
+        });
+        return;
+    }
+
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // const firstName = req.body.firstName;
+    // const lastName = req.body.lastName;
+
+    const { email, password, firstName, lastName } = req.body;
+
+    const hashPassword = await bcrypt.hash(password, 5);
+
+    await adminModel.create({
+        email: email,
+        password: hashPassword,
+        firstName: firstName,
+        lastName: lastName
+    });
+
     res.json({
         message: "signup endpoint",
     });
+
+
 });
 
-adminRouter.post("/signin", (req, res) => {
-  res.json({
-    message: "signin endpoint",
-  });
+adminRouter.post("/signin", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const response = await adminModel.findOne({
+        email: email
+    });
+
+    if (!response) {
+        res.status(403).json({
+            message: "User does not exist in our db"
+        });
+        return;
+    }
+
+    const passwordMathch = await bcrypt.compare(password, response.password);
+
+    if (passwordMathch) {
+        const token = jwt.sign({
+            id: response._id.toString()
+        }, JWT_ADMIN_SECRET);
+
+        res.json({
+            token: token
+        });
+    }
+    else {
+        res.json({
+            message: "Invalid credentials"
+        });
+    }
+
 });
 
 adminRouter.post("/course/", (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
+    creatorId = req.body.creatorId;
     res.json({
         message: "admin course endpoint"
     });
